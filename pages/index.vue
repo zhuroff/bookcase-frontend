ad489bf70be9e9d409f4a6393609ec40e50bc387
zhuroff <ezhuroff@gmail.com>
2021-12-06 15:25:31 +0300

    backups frontend prototype

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Index page</div>
+  <div id="container">
+    <button @click="backup(['books'])">Backup books</button>
+    <button @click="backup(['authors'])">Backup authors</button>
+    <button @click="backup(['genres'])">Backup genres</button>
+    <button @click="backup(['lists'])">Backup lists</button>
+    <button @click="backup(['publishers'])">Backup publishers</button>
+    <button @click="backup(['series'])">Backup series</button>
+    <button @click="backup(backupCollections)">Backup all</button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,19 +19,54 @@ export default Vue.extend({
 
   data() {
     return {
-      obj: {}
+      backupCollections: [
+        'books',
+        'authors',
+        'genres',
+        'lists',
+        'publishers',
+        'series'
+      ],
+
+      readingBooks: {
+        isFetched: false,
+        data: []
+      }
     }
   },
 
   async fetch() {
-    this.fetchTestData()
+    // await this.fetchReadingBooks()
   },
 
   methods: {
-    fetchTestData() {
-      this.$axios.get('/api/test')
-        .then((response: any) => console.log(response.data))
-        .catch((error: any) => console.error(error))
+    async fetchReadingBooks () {
+      try {
+        const response = await this.$axios.get('/api/dashboard/reading-books')
+        console.log(response.data)
+      
+        this.readingBooks = {
+          isFetched: true,
+          data: response.data
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    },
+
+    async backup(collection: string[]) {
+      try {
+        const backupQueryMap = collection.map(async (el: string) => {
+          return await this.$axios.post(`/api/backup/${el}`)
+        })
+
+        const response = await Promise.all(backupQueryMap)
+        const responseMessages = response.map((el) => el.data)
+
+        console.log(responseMessages)
+      } catch (error) {
+        console.error(error)
+      }
     }
   }
 })
