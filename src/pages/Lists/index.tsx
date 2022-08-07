ad7de19f70c5b84a7606af780c3ecb70e925d330
zhuroff <ezhuroff@gmail.com>
2022-08-08 00:58:05 +0300

    lists index: get -> post & paginated

--- src/pages/Lists/index.tsx
+++ src/pages/Lists/index.tsx
@@ -3,41 +3,75 @@ import { Avatar } from 'primereact/avatar';
 import { Badge } from 'primereact/badge';
 import { Card } from 'primereact/card';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
+import { ListActions } from '../../components/ListActions/ListActions';
 import { Preloader } from '../../components/Preloader/Preloader';
 import { useApi } from '../../hooks/useApi';
 import { useLocale } from '../../hooks/useLocale';
-import { TCategoryBasic } from '../../types/Categories';
+import { usePageConfig } from '../../hooks/usePageConfig';
+import { TCategoriesResponse, TCategoryBasic } from '../../types/Categories';
 
 export const Lists = observer(() => {
-  const { get } = useApi()
+  const { post } = useApi()
   const { text } = useLocale()
+  const [searchParams] = useSearchParams()
+  const location = useLocation()
+  const [pageConfig, setPageConfig] = usePageConfig({ pageKey: 'list' })
   const [listFetched, setListFetchedState] = useState(false)
   const [list, setList] = useState<TCategoryBasic[]>([])
 
   const fetchLists = () => {
-    get<TCategoryBasic[]>('/api/lists')
+    post<TCategoriesResponse>('/api/lists', pageConfig)
       .then((response) => {
-        setList(response.data)
+        setList(response.data.docs)
         setListFetchedState(true)
       })
       .catch((error) => console.dir(error))
   }
 
   useEffect(() => {
-    fetchLists()
-  }, [])
+    if (pageConfig) fetchLists()
+  }, [pageConfig])
+
+  useEffect(() => {
+    if (list.length) {
+      setListFetchedState(false)
+      fetchLists()
+    }
+  }, [location.pathname])
+
+  if (!pageConfig) return null
 
   return (
     <>
       <header className="section__heading">
         <h2 className="section__title">{text('routes.lists')}</h2>
+
+        <ListActions
+          isDraft={pageConfig.isDraft}
+          createEntity={() => console.log('Create')}
+          updateConfig={(payload) => {
+            if (payload.isDraft) payload.page = 1
+            setListFetchedState(false)
+            setPageConfig({ ...pageConfig, ...payload })
+          }}
+          resetConfig={() => {
+            setListFetchedState(false)
+            localStorage.removeItem('list')
+            setPageConfig({
+              page: searchParams.get('page') || 1,
+              sort: { title: 1 },
+              limit: 30,
+              isDraft: false
+            })
+          }}
+        />
       </header>
 
       <ul className="cards">
         {
           !listFetched ?
-          <Preloader /> :
+            <Preloader /> :
             !list.length ?
               <li className="cards__empty">{text('common.emptySection')}</li> :
               list.map((item) => (
